68b207651581df2038197d8c0924ef8837cf6dc2
Josip Opacic <josip.opacic@gmail.com>
2019-11-02 18:16:02 +0100

    Create bootcamps route group and mount routers in server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const dotenv = require('dotenv');
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
+// route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
