8c2496f63ad012c70cb2db67013974a06e9c3292
Josip Opacic <josip.opacic@gmail.com>
2019-11-02 22:26:42 +0100

    Add logging middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
@@ -8,6 +9,11 @@ dotenv.config({ path: './config/config.env' });
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// dev loggging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // mount routers
 app.use('/api/v1/bootcamps', bootcamps);
