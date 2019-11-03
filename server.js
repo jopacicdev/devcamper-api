8a582a4309936382e29b46feab25227f3a054b78
Josip Opacic <josip.opacic@gmail.com>
2019-11-04 00:09:21 +0100

    Create DB connection to mongo db via mongoose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to db
+connectDB();
 
 // route files
 const bootcamps = require('./routes/bootcamps');
@@ -20,4 +24,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // Close server and exit process
+    server.close(() => process.exit(1));
+});
