cf6ce8ef2aef1aac6df7ccd21b3aac3ea6bcabc8
Josip Opacic <josip.opacic@gmail.com>
2019-11-04 14:02:09 +0100

    Get one and all bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,10 +5,19 @@ const Bootcamp = require('../models/Bootcamp')
  * @route   GET /api/v1/bootcamps
  * @access  public
  */
-exports.getBootcamps = (req, res, next) => {
-    res
-        .status(200)
-        .json({success: true, msg: 'Show all bootcamps'});
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({
+            success: true,
+            data: bootcamps,
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+        });
+    }
 }
 
 /**
@@ -16,10 +25,19 @@ exports.getBootcamps = (req, res, next) => {
  * @route   GET /api/v1/bootcamps/:id
  * @access  public
  */
-exports.getBootcamp = (req, res, next) => {
-    res
-        .status(200)
-        .json({success: true, msg: 'Get bootcamp '});
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp,
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+        });
+    }
 }
 
 /**
